91601f936a0e589b1047c7a6a8e120c57f44d00e
Jonathan Liu <jwliu0724@hotmail.com>
2024-03-25 14:19:40 -0700

    add play music

--- core/src/ca/bcit/comp2522/termproject/CrowdSurvivor.java
+++ core/src/ca/bcit/comp2522/termproject/CrowdSurvivor.java
@@ -1,18 +1,27 @@
 package ca.bcit.comp2522.termproject;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class CrowdSurvivor extends ApplicationAdapter {
+
 	SpriteBatch batch;
 	Texture img;
-	
+	Music music;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		music = Gdx.audio.newMusic(Gdx.files.internal("testMusic.mp3"));
+		music.setLooping(true);
+		music.play();
+
 	}
 
 	@Override
