5e02de4b7b48cabc2be4030e248c9937be09554b
Jonathan Liu <jwliu0724@hotmail.com>
2024-03-30 21:12:49 -0700

    add viewport variable

--- core/src/ca/bcit/comp2522/termproject/CrowdSurvivor.java
+++ core/src/ca/bcit/comp2522/termproject/CrowdSurvivor.java
@@ -1,30 +1,25 @@
 package ca.bcit.comp2522.termproject;
 
-import ca.bcit.comp2522.termproject.screens.MainMenuScreen;
 import ca.bcit.comp2522.termproject.screens.StartMenuScreen;
-import com.badlogic.gdx.ApplicationAdapter;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class CrowdSurvivor extends Game {
 
-	public int viewportX = 1280;
-	public int viewportY = 720;
+	public int viewportX;
+	public int viewportY;
 	public SpriteBatch batch;
 	public BitmapFont font;
-
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
-
+		viewportX = Gdx.graphics.getWidth();
+		viewportY = Gdx.graphics.getHeight();
 		this.setScreen(new StartMenuScreen(this));
 	}
 
