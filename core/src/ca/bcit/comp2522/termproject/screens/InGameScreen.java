1d101bd77c5f9f45bab5012e7dd859662cb95d1e
Jonathan Liu <jwliu0724@hotmail.com>
2024-03-30 20:06:01 -0700

    rename driver classes to screen class

---
+++ core/src/ca/bcit/comp2522/termproject/screens/InGameScreen.java
@@ -0,0 +1,46 @@
+package ca.bcit.comp2522.termproject.screens;
+
+import com.badlogic.gdx.Screen;
+
+public class InGameScreen implements Screen {
+
+    private static boolean onGameScreen;
+    public static void runGame() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/ca/bcit/comp2522/termproject/screens/ShopScreen.java
@@ -0,0 +1,48 @@
+package ca.bcit.comp2522.termproject.screens;
+
+import com.badlogic.gdx.Screen;
+
+public class ShopScreen implements Screen {
+    private static boolean onShopScreen;
+    public static void runShop() {
+        while (onShopScreen) {
+
+        }
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
