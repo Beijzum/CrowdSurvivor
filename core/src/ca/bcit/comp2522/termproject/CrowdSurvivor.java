06083cf07e0a96688a30ac1e2fe603330288173a
Jonathan Liu <jwliu0724@hotmail.com>
2024-03-30 20:07:18 -0700

    move functionality to individual screens

--- core/src/ca/bcit/comp2522/termproject/CrowdSurvivor.java
+++ core/src/ca/bcit/comp2522/termproject/CrowdSurvivor.java
@@ -1,40 +1,40 @@
 package ca.bcit.comp2522.termproject;
 
+import ca.bcit.comp2522.termproject.screens.MainMenuScreen;
+import ca.bcit.comp2522.termproject.screens.StartMenuScreen;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class CrowdSurvivor extends ApplicationAdapter {
+public class CrowdSurvivor extends Game {
 
-	SpriteBatch batch;
-	Texture img;
-	Music music;
+	public int viewportX = 1280;
+	public int viewportY = 720;
+	public SpriteBatch batch;
+	public BitmapFont font;
 
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		music = Gdx.audio.newMusic(Gdx.files.internal("testMusic.mp3"));
-		music.setLooping(true);
-		music.play();
+		font = new BitmapFont();
 
+		this.setScreen(new StartMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
