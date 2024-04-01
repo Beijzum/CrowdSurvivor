3043fea28e1d482ea74b6ffbf31cead8dcaf4662
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-01 01:18:07 -0700

    move screens to initialize upon game launch

--- core/src/ca/bcit/comp2522/termproject/CrowdSurvivor.java
+++ core/src/ca/bcit/comp2522/termproject/CrowdSurvivor.java
@@ -1,11 +1,15 @@
 package ca.bcit.comp2522.termproject;
 
-import ca.bcit.comp2522.termproject.screens.StartMenuScreen;
+import ca.bcit.comp2522.termproject.screens.*;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import jdk.javadoc.internal.tool.Start;
+import jdk.tools.jmod.Main;
 
 public class CrowdSurvivor extends Game {
 
@@ -13,21 +17,39 @@ public class CrowdSurvivor extends Game {
 	public int viewportY;
 	public SpriteBatch batch;
 	public BitmapFont font;
+	public Stage stage;
+	public MainMenuScreen mainMenuScreen;
+	public StartMenuScreen startMenuScreen;
+	public ShopScreen shopScreen;
+	public LoadoutMenuScreen loadoutMenuScreen;
+	public GachaScreen gachaScreen;
+	public InGameScreen inGameScreen;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
+
 		viewportX = Gdx.graphics.getWidth();
 		viewportY = Gdx.graphics.getHeight();
-		this.setScreen(new StartMenuScreen(this));
+
+		mainMenuScreen = new MainMenuScreen(this);
+		startMenuScreen = new StartMenuScreen(this);
+		shopScreen = new ShopScreen(this);
+		loadoutMenuScreen = new LoadoutMenuScreen(this);
+		gachaScreen = new GachaScreen(this);
+		inGameScreen = new InGameScreen(this);
+
+		this.setScreen(startMenuScreen);
 	}
 
 	@Override
 	public void render () {
 		super.render();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
