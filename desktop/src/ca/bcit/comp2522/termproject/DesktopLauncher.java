91cfc6ceed901317d2925e3dc8f486e86ae6a223
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-01 14:25:45 -0700

    change game resolution

--- desktop/src/ca/bcit/comp2522/termproject/DesktopLauncher.java
+++ desktop/src/ca/bcit/comp2522/termproject/DesktopLauncher.java
@@ -7,8 +7,8 @@ import ca.bcit.comp2522.termproject.CrowdSurvivor;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 
-	private static final int width = 1280;
-	private static final int height = 720;
+	private static final int width = 1920;
+	private static final int height = 1080;
 
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
