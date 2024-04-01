dd6e3476f849a2a63ef148d97fa64ffe2d0902e1
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-01 02:02:45 -0700

    create Projectile class

---
+++ core/src/ca/bcit/comp2522/termproject/Projectile.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2522.termproject;
+
+import com.badlogic.gdx.math.Rectangle;
+
+public class Projectile {
+    private Rectangle hitbox;
+    public Projectile() {
+
+    }
+
+    public void destroyProjectile() {
+
+    }
+}
