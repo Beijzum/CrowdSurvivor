20ea780d8134328c421de74981a299503db7cdc5
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-01 02:05:27 -0700

    start working on player

--- core/src/ca/bcit/comp2522/termproject/Player.java
+++ core/src/ca/bcit/comp2522/termproject/Player.java
@@ -1,16 +1,27 @@
 package ca.bcit.comp2522.termproject;
 
 public class Player extends Entity {
-    private Player instance = null;
+    private static Player instance = null;
+    private int attackSpeed;
+    private float critRate;
+    private float critMultiplier;
+    private int ultimateCD;
+    private float healthRegenMultiplier;
+
 
     Player() {
 
     }
 
-    public Player createPlayer() {
+    public static Player createPlayer() {
         if (instance == null) {
             instance = new Player();
         }
         return instance;
+    }
+
+    @Override
+    public void move(int x, int y) {
+
     }
 }
