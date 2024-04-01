76ad26d341adced02100a83275cb24b2f3b1ebba
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-01 12:02:03 -0700

    remove unused methods

--- core/src/ca/bcit/comp2522/termproject/ActorManager.java
+++ core/src/ca/bcit/comp2522/termproject/ActorManager.java
@@ -2,9 +2,9 @@ package ca.bcit.comp2522.termproject;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.SnapshotArray;
 
 public interface ActorManager {
-    void addActor(Actor actor);
 
     default void addActors(Stage stage, Actor[] actors) {
         for (Actor actor : actors) {
@@ -13,10 +13,9 @@ public interface ActorManager {
     }
 
     default void clearStage(Stage stage) {
-        stage.clear();
-    }
-
-    default void removeActor(Actor actor) {
-        actor.remove();
+        SnapshotArray<Actor> actors = new SnapshotArray<>(stage.getActors());
+        for (Actor actor : actors) {
+            actor.remove();
+        }
     }
 }
