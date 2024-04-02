d1f06b160989c5bda2ca16c10abde0e5a952e4c8
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-02 01:44:43 -0700

    finish methods for shooting projectiles

--- core/src/ca/bcit/comp2522/termproject/Projectile.java
+++ core/src/ca/bcit/comp2522/termproject/Projectile.java
@@ -1,7 +1,6 @@
 package ca.bcit.comp2522.termproject;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
@@ -12,11 +11,20 @@ public class Projectile {
     private int lifetime;
     private Sprite sprite;
     private float lifetimeTimer = 0;
+    final private Vector2 directionVector;
 
     public Projectile(Sprite sprite, int speed, int lifetime) {
         this.sprite = sprite;
         this.speed = speed;
         this.lifetime = lifetime;
+        this.directionVector = new Vector2();
+    }
+
+    public Projectile(Projectile projectileTemplate) {
+        this.speed = projectileTemplate.getSpeed();
+        this.lifetime = projectileTemplate.getLifetime();
+        this.directionVector = new Vector2(projectileTemplate.getDirectionVector());
+        this.sprite = new Sprite(projectileTemplate.getSprite());
     }
 
     public int getSpeed() {
@@ -30,7 +38,12 @@ public class Projectile {
     public Rectangle getHitbox() {
         return this.sprite.getBoundingRectangle();
     }
-
+    public Vector2 getDirectionVector() {
+        return this.directionVector;
+    }
+    public int getLifetime() {
+        return lifetime;
+    }
     public void setSpeed(int speed) {
         this.speed = speed;
     }
@@ -43,16 +56,11 @@ public class Projectile {
         this.lifetime = lifetime;
     }
 
-    public void setX(float x) {
-        this.sprite.setX(x);
-    }
-
-    public void setY(float y) {
-        this.sprite.setY(y);
-    }
-
     public void setSpriteOrigin(float x, float y) {
         this.sprite.setOrigin(x, y);
+    }
+    public void setProjectileCenter(float x, float y) {
+        this.sprite.setCenter(x, y);
     }
 
     public void setSpriteRotation(float angleDegrees) {
@@ -70,19 +78,38 @@ public class Projectile {
         this.lifetimeTimer += Gdx.graphics.getDeltaTime();
     }
 
-    public Projectile copyProjectile() {
-        return new Projectile(this.sprite, this.speed, this.lifetime);
-    }
-
     public void draw(Batch batch) {
-        batch.begin();
+        moveProjectile();
         batch.draw(this.sprite, this.sprite.getX(), this.sprite.getY(), this.sprite.getOriginX(),
                 this.sprite.getOriginY(), this.sprite.getWidth(), this.sprite.getHeight(), this.sprite.getScaleX(),
                 this.sprite.getScaleY(), this.sprite.getRotation());
-        batch.end();
     }
 
-    public void moveProjectile(float x, float y) {
+    public void moveProjectile() {
+        float angle = this.directionVector.angleRad();
 
+        // vector already normalized
+        float deltaX = this.speed * Gdx.graphics.getDeltaTime() * (float) Math.cos(angle);
+        float deltaY = this.speed * Gdx.graphics.getDeltaTime() * (float) Math.sin(angle);
+        this.setProjectileCenter(this.getCenterX() + deltaX, this.getCenterY() + deltaY);
+    }
+
+    private float getCenterX() {
+        return this.sprite.getX() + this.sprite.getWidth() / 2;
+    }
+
+    private float getCenterY() {
+        return this.sprite.getY() + this.sprite.getHeight() / 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Projectile{" +
+                "speed=" + speed +
+                ", lifetime=" + lifetime +
+                ", sprite=" + sprite +
+                ", lifetimeTimer=" + lifetimeTimer +
+                ", directionVector=" + directionVector +
+                '}';
     }
 }
