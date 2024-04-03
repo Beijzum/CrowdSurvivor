9200f6f22e9c8e01e67759212396426617c3242c
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-03 07:09:30 -0700

    create new classes for each possible state in game

---
+++ core/src/ca/bcit/comp2522/termproject/screens/PauseMenuScreen.java
@@ -0,0 +1,41 @@
+package ca.bcit.comp2522.termproject.screens;
+
+import com.badlogic.gdx.Screen;
+
+public class PauseMenuScreen implements Screen {
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/ca/bcit/comp2522/termproject/screens/UpgradeSelectionScreen.java
@@ -0,0 +1,40 @@
+package ca.bcit.comp2522.termproject.screens;
+
+import com.badlogic.gdx.Screen;
+
+public class UpgradeSelectionScreen implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
