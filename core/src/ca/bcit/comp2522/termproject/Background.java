0736bee519363610632d09abf1bcb91ae99191d2
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-03 12:09:12 -0700

    disable blending on background to improve performance

--- core/src/ca/bcit/comp2522/termproject/Background.java
+++ core/src/ca/bcit/comp2522/termproject/Background.java
@@ -4,12 +4,12 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public interface Background {
     default void renderBackground(CrowdSurvivor game, Sprite background) {
-        background.setSize(game.viewportX, game.viewportY);
-
+        game.batch.disableBlending();
         game.batch.begin();
         game.batch.draw(background, background.getX(), background.getY(), background.getOriginX(),
                 background.getOriginY(), background.getWidth(), background.getHeight(), background.getScaleX(),
                 background.getScaleY(), background.getRotation());
         game.batch.end();
+        game.batch.enableBlending();
     }
 }
