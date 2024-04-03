e85d5f7422c0871580e7ab7b686c8844136bb9cd
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-03 12:52:11 -0700

    finish pause menu and make class implement input processor

--- core/src/ca/bcit/comp2522/termproject/screens/PauseMenuScreen.java
+++ core/src/ca/bcit/comp2522/termproject/screens/PauseMenuScreen.java
@@ -2,29 +2,29 @@ package ca.bcit.comp2522.termproject.screens;
 
 import ca.bcit.comp2522.termproject.ActorManager;
 import ca.bcit.comp2522.termproject.CrowdSurvivor;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
-public class PauseMenuScreen implements Screen, ActorManager {
-    final int numberOfButtons = 3;
+public class PauseMenuScreen implements Screen, ActorManager, InputProcessor {
+    final int numberOfButtons = 2;
     final private CrowdSurvivor game;
-    final private TextButton[] menuItems = new TextButton[numberOfButtons]; // resume, see player stats, quit
+    final private TextButton[] menuItems = new TextButton[numberOfButtons]; // resume, quit
+    final private InputMultiplexer inputManager;
 
     public PauseMenuScreen(CrowdSurvivor game) {
         this.game = game;
+        this.inputManager = new InputMultiplexer(this, game.stageUI);
         createButtons();
     }
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(game.stageUI);
         addActors(game.stageUI, menuItems);
+        Gdx.input.setInputProcessor(this.inputManager);
     }
 
     @Override
@@ -64,42 +64,24 @@ public class PauseMenuScreen implements Screen, ActorManager {
         int buttonWidth = Gdx.graphics.getWidth() / 3;
         int buttonHeight = Gdx.graphics.getHeight() / 10;
         int buttonPositionX = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
-        int firstButtonPositionY = Gdx.graphics.getHeight() / 3;
+        int firstButtonPositionY = Gdx.graphics.getHeight() / 2;
 
         // start game button
-        TextButton startGameButton = new TextButton("Start Game", game.skin);
-        startGameButton.setSize(buttonWidth, buttonHeight);
-        startGameButton.setPosition(buttonPositionX, firstButtonPositionY);
-        startGameButton.addListener(new InputListener() {
+        TextButton resumeGameButton = new TextButton("Resume", game.skin);
+        resumeGameButton.setSize(buttonWidth, buttonHeight);
+        resumeGameButton.setPosition(buttonPositionX, firstButtonPositionY);
+        resumeGameButton.addListener(new InputListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 if (button != Input.Buttons.LEFT) {
                     return false;
                 }
-//                game.setScreen(game.loadoutMenuScreen); TO IMPLEMENT LATER, GET THE GAME DONE FIRST
+                clearStage(game.stageUI);
                 game.setScreen(game.inGameScreen);
                 return true;
             }
         });
-        this.menuItems[0] = startGameButton;
-
-        // shop button
-        TextButton shopButton = new TextButton("Shop", game.skin);
-        shopButton.setSize(buttonWidth, buttonHeight);
-        shopButton.setPosition(buttonPositionX, firstButtonPositionY - buttonHeight);
-        shopButton.addListener(new InputListener() {
-
-
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                if (button != Input.Buttons.LEFT) {
-                    return false;
-                }
-                game.setScreen(game.shopScreen);
-                return true;
-            }
-        });
-        this.menuItems[1] = shopButton;
+        this.menuItems[0] = resumeGameButton;
 
         // quit game button
         TextButton quitButton = new TextButton("Quit", game.skin);
@@ -111,10 +93,63 @@ public class PauseMenuScreen implements Screen, ActorManager {
                 if (button != Input.Buttons.LEFT) {
                     return false;
                 }
-                Gdx.app.exit();
+                game.inGameScreen.resetGameState();
+                clearStage(game.stageUI);
+                game.inGameScreen.dispose();
+                game.setScreen(game.mainMenuScreen);
                 return true;
             }
         });
-        this.menuItems[2] = quitButton;
+        this.menuItems[1] = quitButton;
+    }
+
+    @Override
+    public boolean keyDown(int keyCode) {
+        if (keyCode == Input.Keys.ESCAPE) {
+            clearStage(game.stageUI);
+            game.setScreen(game.inGameScreen);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int i, int i1, int i2) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float v, float v1) {
+        return false;
     }
 }
