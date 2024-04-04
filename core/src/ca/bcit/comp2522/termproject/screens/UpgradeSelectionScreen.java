eea2a7f2ca8aed228d5ac56a2cb4b016f3bbfcb9
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-04 12:35:38 -0700

    begin layout of how upgrade system

--- core/src/ca/bcit/comp2522/termproject/screens/UpgradeSelectionScreen.java
+++ core/src/ca/bcit/comp2522/termproject/screens/UpgradeSelectionScreen.java
@@ -4,10 +4,26 @@ import ca.bcit.comp2522.termproject.CrowdSurvivor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
+import java.util.Random;
+
 public class UpgradeSelectionScreen implements Screen {
     final int numberOfButtons = 4;
     final private CrowdSurvivor game;
     final private TextButton[] menuItems = new TextButton[numberOfButtons]; // different upgrades to choose from
+    final private Random randomNumberGenerator = new Random();
+    final private String[] possibleUpgrades = {
+            "+10 Speed", // +10
+            "+10 Attack", // +10
+            "+10 Health", // +10
+            "+10% More Experience", // *1.1
+            "+10% More Currency", // *1.1
+            "+10% Attack Speed", // *1.1
+            "+10% Longer Invincibility After Taking Damage", // *1.1
+            "+10% Critical Damage Multiplier", // +0.1
+            "+5% Critical Hit Rate", // +0.05
+            "+5 Defense", // +0.05
+            "+5% Health Regeneration" // +0.05
+    };
 
     public UpgradeSelectionScreen(CrowdSurvivor game) {
         this.game = game;
