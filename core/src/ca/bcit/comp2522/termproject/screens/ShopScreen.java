ef3264e3519d3520bee73a5ab130271a8c27499b
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-05 07:59:45 -0700

    finish creating shop menu buttons

--- core/src/ca/bcit/comp2522/termproject/screens/ShopScreen.java
+++ core/src/ca/bcit/comp2522/termproject/screens/ShopScreen.java
@@ -1,23 +1,57 @@
 package ca.bcit.comp2522.termproject.screens;
 
+import ca.bcit.comp2522.termproject.ActorManager;
+import ca.bcit.comp2522.termproject.Background;
 import ca.bcit.comp2522.termproject.CrowdSurvivor;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.ScreenUtils;
+import org.w3c.dom.Text;
 
-public class ShopScreen implements Screen {
-    private static boolean onShopScreen;
-
+public class ShopScreen implements Screen, Background, ActorManager {
+    final private static int BASE_ITEM_PRICE = 500;
+    final private CrowdSurvivor game;
+    final private Music music;
+    final private TextButton[] menuItems;
+    final private TextButton.TextButtonStyle shopButtonStyle;
+    final private Color backgroundFilter = new Color(50 / 255f, 50 / 255f, 100 / 255f, 1);
+    final private Sprite background = new Sprite(new Texture("backgrounds/shopMenuBackground.jpg"));
     public ShopScreen(CrowdSurvivor crowdSurvivor) {
-
+        this.game = crowdSurvivor;
+        this.background.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        this.music = Gdx.audio.newMusic(Gdx.files.internal("music/shopMenuMusic.mp3"));
+        this.shopButtonStyle = new TextButton.TextButtonStyle();
+        this.shopButtonStyle.downFontColor = Color.GREEN;
+        this.shopButtonStyle.checkedFontColor = Color.RED;
+        this.menuItems = createButtons();
     }
 
     @Override
     public void show() {
+        Gdx.input.setInputProcessor(game.buttonsUI);
+        music.setLooping(true);
+        music.play();
 
+        addActors(game.buttonsUI, menuItems);
     }
 
     @Override
     public void render(float v) {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
 
+        renderBackgroundWithFilter(game, background, backgroundFilter);
+        game.buttonsUI.act();
+        game.buttonsUI.draw();
     }
 
     @Override
@@ -37,11 +71,259 @@ public class ShopScreen implements Screen {
 
     @Override
     public void hide() {
-
+        clearStage(game.buttonsUI);
+        dispose();
     }
 
     @Override
     public void dispose() {
+        music.dispose();
+    }
 
+    private TextButton[] createButtons() {
+        int buyAttackPrice = BASE_ITEM_PRICE;
+        TextButton buyAttack = new TextButton(String.format("%d: Buy Attack", buyAttackPrice), game.skin);
+        buyAttack.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+
+                if (game.playerProfile.getCurrency() < buyAttackPrice) {
+                    // playSFX(noMoneySFX);
+                    return true;
+                }
+                // playSFX(purchaseSFX);
+                game.playerProfile.setAttackBooster(game.playerProfile.getAttackBooster() + 5);
+                game.playerProfile.setCurrency(game.playerProfile.getCurrency() - buyAttackPrice);
+                return true;
+            }
+        });
+
+        int buyHealthPrice = BASE_ITEM_PRICE;
+        TextButton buyHealth = new TextButton(String.format("%d: Buy Health", buyHealthPrice), game.skin);
+        buyHealth.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+
+                if (game.playerProfile.getCurrency() < buyHealthPrice) {
+                    // playSFX(noMoneySFX);
+                    return true;
+                }
+                // playSFX(purchaseSFX);
+                game.playerProfile.setMaxHealthBooster(game.playerProfile.getMaxHealthBooster() + 5);
+                game.playerProfile.setCurrency(game.playerProfile.getCurrency() - buyHealthPrice);
+                return true;
+            }
+        });
+
+        int buySpeedPrice = BASE_ITEM_PRICE;
+        TextButton buySpeed = new TextButton(String.format("%d: Buy Speed", buySpeedPrice), game.skin);
+        buySpeed.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+
+                if (game.playerProfile.getCurrency() < buySpeedPrice) {
+                    // playSFX(noMoneySFX);
+                    return true;
+                }
+                // playSFX(purchaseSFX);
+                game.playerProfile.setSpeedBooster(game.playerProfile.getSpeedBooster() + 5);
+                game.playerProfile.setCurrency(game.playerProfile.getCurrency() - buySpeedPrice);
+                return true;
+            }
+        });
+
+        int buyDefensePrice = Math.round(BASE_ITEM_PRICE * 1.5f);
+        TextButton buyDefense = new TextButton(String.format("%d: Buy Defense", buyDefensePrice), game.skin);
+        buyDefense.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+
+                if (game.playerProfile.getCurrency() < buyDefensePrice) {
+                    // playSFX(noMoneySFX);
+                    return true;
+                }
+                // playSFX(purchaseSFX);
+                game.playerProfile.setDefenseBooster(game.playerProfile.getDefenseBooster() + 0.02f);
+                game.playerProfile.setCurrency(game.playerProfile.getCurrency() - buyDefensePrice);
+                return true;
+            }
+        });
+
+        int buyEXPPrice = BASE_ITEM_PRICE;
+        TextButton buyEXP = new TextButton(String.format("%d: Buy Base EXP Gain", buyEXPPrice), game.skin);
+        buyEXP.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+
+                if (game.playerProfile.getCurrency() < buyEXPPrice) {
+                    // playSFX(noMoneySFX);
+                    return true;
+                }
+                // playSFX(purchaseSFX);
+                game.playerProfile.setEXPMultiplierBooster(game.playerProfile.getEXPMultiplierBooster() + 0.05f);
+                game.playerProfile.setCurrency(game.playerProfile.getCurrency() - buyEXPPrice);
+                return true;
+            }
+        });
+
+        int buyCurrencyPrice = BASE_ITEM_PRICE * 2;
+        TextButton buyCurrency = new TextButton(
+                String.format("%d: Buy Base Currency Gain", buyCurrencyPrice), game.skin);
+        buyCurrency.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+
+                if (game.playerProfile.getCurrency() < buyCurrencyPrice) {
+                    // playSFX(noMoneySFX);
+                    return true;
+                }
+                // playSFX(purchaseSFX);
+                game.playerProfile
+                        .setCurrencyMultiplierBooster(game.playerProfile.getCurrencyMultiplierBooster() + 0.05f);
+                game.playerProfile.setCurrency(game.playerProfile.getCurrency() - buyCurrencyPrice);
+                return true;
+            }
+        });
+
+        int buyAtttackSpeedPrice = Math.round(BASE_ITEM_PRICE * 1.75f);
+        TextButton buyAttackSpeed = new TextButton(
+                String.format("%d: Buy Attack Speed", buyAtttackSpeedPrice), game.skin);
+        buyAttackSpeed.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+
+                if (game.playerProfile.getCurrency() < buyAtttackSpeedPrice) {
+                    // playSFX(noMoneySFX);
+                    return true;
+                }
+                // playSFX(purchaseSFX);
+                game.playerProfile.setAttackSpeedBooster(game.playerProfile.getAttackSpeedBooster() - 0.05f);
+                game.playerProfile.setCurrency(game.playerProfile.getCurrency() - buyAtttackSpeedPrice);
+                return true;
+            }
+        });
+
+        int buyCritRatePrice = BASE_ITEM_PRICE * 2;
+        TextButton buyCritRate = new TextButton(
+                String.format("%d: Buy Critical Hit Rate", buyCritRatePrice), game.skin);
+        buyCritRate.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+
+                if (game.playerProfile.getCurrency() < buyCritRatePrice) {
+                    // playSFX(noMoneySFX);
+                    return true;
+                }
+                // playSFX(purchaseSFX);
+                game.playerProfile.setCritRateBooster(game.playerProfile.getCritRateBooster() + 0.02f);
+                game.playerProfile.setCurrency(game.playerProfile.getCurrency() - buyCritRatePrice);
+                return true;
+            }
+        });
+
+        int buyCritDamagePrice = BASE_ITEM_PRICE * 2;
+        TextButton buyCritDamage = new TextButton(
+                String.format("%d: Buy Critical Hit Damage", buyCritDamagePrice), game.skin);
+        buyCritDamage.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+
+                if (game.playerProfile.getCurrency() < buyCritDamagePrice) {
+                    // playSFX(noMoneySFX);
+                    return true;
+                }
+                // playSFX(purchaseSFX);
+                game.playerProfile.setCritMultiplierBooster(game.playerProfile.getCritMultiplierBooster() + 0.05f);
+                game.playerProfile.setCurrency(game.playerProfile.getCurrency() - buyCritDamagePrice);
+                return true;
+            }
+        });
+
+        int buyHealthRegenPrice = BASE_ITEM_PRICE;
+        TextButton buyHealthRegen = new TextButton(
+                String.format("%d: Buy Health Regeneration", buyHealthRegenPrice), game.skin);
+        buyHealthRegen.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+
+                if (game.playerProfile.getCurrency() < buyHealthRegenPrice) {
+                    // playSFX(noMoneySFX);
+                    return true;
+                }
+                // playSFX(purchaseSFX);
+                game.playerProfile
+                        .setHealthRegenMultiplierBooster(game.playerProfile.getHealthRegenMultiplierBooster() + 0.02f);
+                game.playerProfile.setCurrency(game.playerProfile.getCurrency() - buyHealthPrice);
+                return true;
+            }
+        });
+
+        TextButton backButton = new TextButton("Back To Menu", game.skin);
+        backButton.addListener(new InputListener() {
+           @Override
+           public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+               if (button != Input.Buttons.LEFT) {
+                   return false;
+               }
+               dispose();
+               game.setScreen(game.mainMenuScreen);
+               return true;
+           }
+        });
+
+        return new TextButton[] {
+                buyAttack, buySpeed, buyHealth, buyHealthRegen, buyEXP, buyAttackSpeed, buyDefense, buyCritRate,
+                buyCritDamage, buyCurrency, backButton
+        };
+    }
+
+    private void adjustButtonColor(int price, TextButton button) {
+        if (game.playerProfile.getCurrency() < price) {
+            button.setColor(175 / 255f, 25 / 255f, 25 / 255f, 1);
+        }
+    }
+
+    private void playSFX() {
+        // implement later
+    }
+
+    private void positionButton(TextButton button, int buttonNumber) {
+        int buttonWidth = Gdx.graphics.getWidth() / 3;
+        int buttonHeight = Gdx.graphics.getHeight() / 6;
+        int buttonPositionX = Gdx.graphics.getWidth() / 2 - buttonWidth / 2;
+        int buttonPositionY = Gdx.graphics.getHeight() * 2 / 3 - buttonHeight * buttonNumber;
+
+        button.setSize(buttonWidth, buttonHeight);
+        button.setPosition(buttonPositionX, buttonPositionY);
     }
 }
