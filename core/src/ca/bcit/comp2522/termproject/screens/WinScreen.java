ef57643292ba53746627c48e1ca8883874489abc
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-05 13:10:34 -0700

    begin making win screen

---
+++ core/src/ca/bcit/comp2522/termproject/screens/WinScreen.java
@@ -0,0 +1,48 @@
+package ca.bcit.comp2522.termproject.screens;
+
+import ca.bcit.comp2522.termproject.CrowdSurvivor;
+import com.badlogic.gdx.Screen;
+
+public class WinScreen implements Screen {
+
+    public CrowdSurvivor game;
+
+    public WinScreen(CrowdSurvivor game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
