84c3c0a7377f880b9ae16808c4e56d217f850ce3
Jason Chow <jchow149@my.bcit.ca>
2024-04-06 17:23:52 -0700

    ADD EXP level text

--- core/src/ca/bcit/comp2522/termproject/EXPBar.java
+++ core/src/ca/bcit/comp2522/termproject/EXPBar.java
@@ -1,28 +1,33 @@
 package ca.bcit.comp2522.termproject;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
+
+import static ca.bcit.comp2522.termproject.CrowdSurvivor.font;
 
 public class EXPBar {
     private float x, y;
     private final float width;
     private final float height;
-    private int levelthreshold;
+    private int levelThreshold;
     private int currentEXP;
     private final Color backgroundColor;
     private final Color foregroundColor;
+    private int playerLevel;
 
-    public EXPBar(float x, float y, float width, float height, int levelthreshold,
+    public EXPBar(float x, float y, float width, float height, int playerLevel, int levelThreshold,
                   Color backgroundColor, Color foregroundColor) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.levelthreshold = levelthreshold;
+        this.levelThreshold = levelThreshold;
         this.currentEXP = 0;
         this.backgroundColor = backgroundColor;
         this.foregroundColor = foregroundColor;
+        this.playerLevel = playerLevel;
     }
 
     public float getWidth() {
@@ -41,22 +46,33 @@ public class EXPBar {
 
     public void setCurrentEXP(int currentEXP) {
         // set current experience
-        this.currentEXP = MathUtils.clamp(currentEXP, 0, this.levelthreshold);
+        this.currentEXP = MathUtils.clamp(currentEXP, 0, this.levelThreshold);
     }
 
     public void setMaxEXP(int maxEXP) {
         // set max experience
-        this.levelthreshold = maxEXP;
+        this.levelThreshold = maxEXP;
     }
 
-    public void draw(ShapeRenderer shapeRenderer) {
+    public void setCurrentLevel(int currentLevel) {
+        this.playerLevel = currentLevel;
+    }
+
+    public void draw(ShapeRenderer shapeRenderer, Batch batch) {
         // draw background
         shapeRenderer.setColor(this.backgroundColor);
         shapeRenderer.rect(this.x, this.y, this.width, this.height);
 
         // draw foreground (experience)
-        float foregroundWidth = (float) this.currentEXP / this.levelthreshold * this.width;
+        float foregroundWidth = (float) this.currentEXP / this.levelThreshold * this.width;
         shapeRenderer.setColor(this.foregroundColor);
         shapeRenderer.rect(this.x, this.y, foregroundWidth, this.height);
+
+        batch.begin();
+        float textX = (float) (this.x + this.getWidth() / 1.05);
+        float textY = this.y + this.getHeight() / 2 + 5;
+        font.setColor(Color.WHITE); // Set font color
+        font.draw(batch, "Level: " + this.playerLevel, textX, textY);
+        batch.end();
     }
 }
