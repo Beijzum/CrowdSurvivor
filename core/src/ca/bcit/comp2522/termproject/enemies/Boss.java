089a32721b873ea397a9527319c6c25c6dadb8e9
Jonathan Liu <jwliu0724@hotmail.com>
2024-04-08 00:24:07 -0700

    implement boss functionality

--- core/src/ca/bcit/comp2522/termproject/enemies/Boss.java
+++ core/src/ca/bcit/comp2522/termproject/enemies/Boss.java
@@ -1,7 +1,35 @@
 package ca.bcit.comp2522.termproject.enemies;
 
+import ca.bcit.comp2522.termproject.Projectile;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+import java.util.LinkedList;
+
 public class Boss extends Enemy {
-    public Boss() {
-        super();
+    final private static Sprite PROJECTILE_SPRITE = new Sprite(
+            new Texture(Gdx.files.internal("projectiles/bossProjectile.png")));
+    final private float attackSpeed;
+    private final Projectile projectileTemplate;
+    private float attackTimer = 0;
+    public Boss(int health, int speed, int attack, int size, String filepath) {
+        super(health, speed, attack, filepath);
+        this.sprite.setSize(size, size);
+        this.attackSpeed = 10f;
+        this.projectileTemplate = new Projectile(PROJECTILE_SPRITE, 125, 10, 200);
+    }
+
+    public void fireProjectile(LinkedList<Projectile> enemyProjectiles, float playerCoordX, float playerCoordY) {
+        if (this.attackTimer >= this.attackSpeed) {
+            Projectile newProjectile = new Projectile(this.projectileTemplate);
+            newProjectile.getDirectionVector().set(playerCoordX - this.getCenterX(), playerCoordY - this.getCenterY());
+            newProjectile.setProjectileCenter(this.getCenterX(), this.getCenterY());
+            enemyProjectiles.add(newProjectile);
+
+            this.attackTimer = 0;
+        } else {
+            this.attackTimer += Gdx.graphics.getDeltaTime();
+        }
     }
 }
