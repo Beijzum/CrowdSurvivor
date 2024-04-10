ba36c939365415bc67732c2a53f38c8199ced03d
Jason Chow <jchow149@my.bcit.ca>
2024-04-09 17:48:05 -0700

    REFACTOR atlas to getatlas()

--- core/src/ca/bcit/comp2522/termproject/enemies/EnemyManager.java
+++ core/src/ca/bcit/comp2522/termproject/enemies/EnemyManager.java
@@ -270,8 +270,8 @@ final public class EnemyManager {
         int speed = BASE_ENEMY_SPEED + Math.round(gameScreen.getTimeElapsed() / 5);
         int size = BASE_BOSS_SIZE + 25 * Math.round(gameScreen.getTimeElapsed() / 30) - 25;
         int attack = BASE_BOSS_ATTACK + 5 * Math.round(gameScreen.getTimeElapsed() / 30) - 10;
-        Boss newBoss = new Boss(health, speed, attack, size, gameScreen.atlas.createSprite("enemies/boss"),
-                gameScreen.atlas.createSprite("projectiles/bossProjectile"));
+        Boss newBoss = new Boss(health, speed, attack, size, gameScreen.getAtlas().createSprite("enemies/boss"),
+                gameScreen.getAtlas().createSprite("projectiles/bossProjectile"));
         newBoss.setCenterPosition(xCoord, yCoord);
         return newBoss;
     }
@@ -281,7 +281,7 @@ final public class EnemyManager {
         int health = Math.round(BASE_CHARGER_HEALTH + gameScreen.getTimeElapsed() / 5);
         int acceleration = 100;
         int attack = 10;
-        Charger newCharger = new Charger(health, acceleration, attack, gameScreen.atlas.createSprite("enemies/charger"));
+        Charger newCharger = new Charger(health, acceleration, attack, gameScreen.getAtlas().createSprite("enemies/charger"));
         newCharger.setCenterPosition(xCoord, yCoord);
         return newCharger;
     }
@@ -292,18 +292,18 @@ final public class EnemyManager {
         switch (choice) {
             case 0:
                 newEnemy = RangedEnemy.createSniper(gameScreen.getTimeElapsed(),
-                        gameScreen.atlas.createSprite("enemies/sniper"),
-                        gameScreen.atlas.createSprite("projectiles/enemyProjectile"));
+                        gameScreen.getAtlas().createSprite("enemies/sniper"),
+                        gameScreen.getAtlas().createSprite("projectiles/enemyProjectile"));
                 break;
             case 1:
                 newEnemy = RangedEnemy.createSpewer(gameScreen.getTimeElapsed(),
-                        gameScreen.atlas.createSprite("enemies/spewer"),
-                        gameScreen.atlas.createSprite("projectiles/enemyProjectile"));
+                        gameScreen.getAtlas().createSprite("enemies/spewer"),
+                        gameScreen.getAtlas().createSprite("projectiles/enemyProjectile"));
                 break;
             default:
                 newEnemy = RangedEnemy.createBasic(gameScreen.getTimeElapsed(),
-                        gameScreen.atlas.createSprite("enemies/basicRanged"),
-                        gameScreen.atlas.createSprite("projectiles/enemyProjectile"));
+                        gameScreen.getAtlas().createSprite("enemies/basicRanged"),
+                        gameScreen.getAtlas().createSprite("projectiles/enemyProjectile"));
                 break;
         }
         newEnemy.setCenterPosition(xCoord, yCoord);
@@ -324,7 +324,7 @@ final public class EnemyManager {
         int health = Math.round(BASE_BASIC_ENEMY_HEALTH + gameScreen.getTimeElapsed() / 5); // temp scaling
         int speed = Math.round(BASE_ENEMY_SPEED + gameScreen.getTimeElapsed() / 5);
         int attack = 10;
-        Enemy newEnemy = new Enemy(health, speed, attack, gameScreen.atlas.createSprite("enemies/enemy"));
+        Enemy newEnemy = new Enemy(health, speed, attack, gameScreen.getAtlas().createSprite("enemies/enemy"));
         newEnemy.setCenterPosition(xCoord, yCoord);
         return newEnemy;
     }
