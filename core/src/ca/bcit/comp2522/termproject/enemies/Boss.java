159b343e9d37f556125710f197822b0efb56b6bb
Jason Chow <jchow149@my.bcit.ca>
2024-04-09 23:01:15 -0700

    ADD javadocs to Boss.java

--- core/src/ca/bcit/comp2522/termproject/enemies/Boss.java
+++ core/src/ca/bcit/comp2522/termproject/enemies/Boss.java
@@ -6,10 +6,33 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 import java.util.LinkedList;
 
+/**
+ * Represents the Boss class, a stronger variant of the basic Enemy.
+ * Extends the Enemy class and encapsulates the behavior and attributes of a Boss enemy.
+ * Inherits from the Entity class.
+ *
+ * @author Jonathan Liu
+ * @author A01375621
+ * @author jwl0724
+ * @author Jason Chow
+ * @author A00942129
+ * @author Beijzum
+ * @version 2024
+ */
 public class Boss extends Enemy {
     private final Projectile projectileTemplate;
     private float attackTimer = 0;
 
+    /**
+     * Constructs a Boss object with custom parameters.
+     *
+     * @param health           int representing the health points of the boss.
+     * @param speed            int representing the speed of the boss.
+     * @param attack           int representing the attack points of the boss.
+     * @param size             int representing the size of the boss's sprite.
+     * @param sprite           Sprite object representing the boss's sprite.
+     * @param projectileSprite Sprite object representing the projectile's sprite.
+     */
     public Boss(final int health, final int speed, final int attack, final int size, final Sprite sprite,
                 final Sprite projectileSprite) {
         super(health, speed, attack, sprite);
@@ -20,9 +43,17 @@ public class Boss extends Enemy {
         this.projectileTemplate = new Projectile(projectileSprite, projectileSpeed, projectileLifetime, projectileSize);
     }
 
+    /**
+     * Fires a projectile towards the player's position.
+     *
+     * @param enemyProjectiles LinkedList of Projectile objects representing enemy projectiles.
+     * @param playerCoordX     float representing the x-coordinate of the player's position.
+     * @param playerCoordY     float representing the y-coordinate of the player's position.
+     * @return true if a projectile is fired, false otherwise.
+     */
     public boolean fireProjectile(final LinkedList<Projectile> enemyProjectiles, final float playerCoordX,
                                   final float playerCoordY) {
-        final float attackSpeed = 10f;
+        final float attackSpeed = 10f; // atk speed in seconds
         if (this.attackTimer >= attackSpeed) {
             Projectile newProjectile = new Projectile(this.projectileTemplate);
             newProjectile.getDirectionVector().set(playerCoordX - this.getCenterX(), playerCoordY - this.getCenterY());
