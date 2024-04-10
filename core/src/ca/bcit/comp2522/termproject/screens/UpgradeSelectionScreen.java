04d9115df3f7985305854cf6d60b5feadc3665b1
Jason Chow <jchow149@my.bcit.ca>
2024-04-09 17:16:47 -0700

    REFACTOR UpgradeSelectionScreen.java to be consistent with checkstyles

--- core/src/ca/bcit/comp2522/termproject/screens/UpgradeSelectionScreen.java
+++ core/src/ca/bcit/comp2522/termproject/screens/UpgradeSelectionScreen.java
@@ -18,11 +18,31 @@ public class UpgradeSelectionScreen implements Screen, ActorManager, Background 
     private final Random randomNumberGenerator = new Random();
     private final TextButton[] possibleUpgrades;
     private final TextButton attackUpgrade;
+    private final TextButton speedUpgrade;
+    private final TextButton healthUpgrade;
+    private final TextButton expUpgrade;
+    private final TextButton currencyUpgrade;
+    private final TextButton attackSpeedUpgrade;
+    private final TextButton iFrameUpgrade;
+    private final TextButton critDamageUpgrade;
+    private final TextButton critRateUpgrade;
+    private final TextButton defenseUpgrade;
+    private final TextButton healthRegenUpgrade;
 
     public UpgradeSelectionScreen(final CrowdSurvivor game) {
         this.game = game;
-        this.possibleUpgrades = createButtons();
         this.attackUpgrade = new TextButton("+10 Attack", this.game.getSkin());
+        this.speedUpgrade = new TextButton("+10 Speed", this.game.getSkin());
+        this.healthUpgrade = new TextButton("+10 Health", this.game.getSkin());
+        this.expUpgrade = new TextButton("+10% More Experience", this.game.getSkin());
+        this.currencyUpgrade = new TextButton("+10% More Currency", this.game.getSkin());
+        this.attackSpeedUpgrade = new TextButton("+10% More Attack Speed", this.game.getSkin());
+        this.iFrameUpgrade = new TextButton("+10% Longer Invincibility After Taking Damage", this.game.getSkin());
+        this.critDamageUpgrade = new TextButton("+10% Critical Hit Damage", this.game.getSkin());
+        this.critRateUpgrade = new TextButton("+5% Critical Hit Rate", this.game.getSkin());
+        this.defenseUpgrade = new TextButton("+5 Defense", this.game.getSkin());
+        this.healthRegenUpgrade = new TextButton("+5% Health Regeneration", this.game.getSkin());
+        this.possibleUpgrades = createButtons();
     }
 
     @Override
@@ -87,187 +107,25 @@ public class UpgradeSelectionScreen implements Screen, ActorManager, Background 
 
     private TextButton[] createButtons() {
 
-        handleAttackUpdate();
-
-
-        TextButton speedUpgrade = new TextButton("+10 Speed", this.game.getSkin());
-        TextButton healthUpgrade = new TextButton("+10 Health", this.game.getSkin());
-        TextButton expUpgrade = new TextButton("+10% More Experience", this.game.getSkin());
-        TextButton currencyUpgrade = new TextButton("+10% More Currency", this.game.getSkin());
-        TextButton attackSpeedUpgrade = new TextButton("+10% More Attack Speed", this.game.getSkin());
-        TextButton iFrameUpgrade = new TextButton("+10% Longer Invicibility After Taking Damage", this.game.getSkin());
-        TextButton critDamageUpgrade = new TextButton("+10% Critical Hit Damage", this.game.getSkin());
-        TextButton critRateUpgrade = new TextButton("+5% Critical Hit Rate", this.game.getSkin());
-        TextButton defenseUpgrade = new TextButton("+5 Defense", this.game.getSkin());
-        TextButton healthRegenUpgrade = new TextButton("+5% Health Regeneration", this.game.getSkin());
-
-        speedUpgrade.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
-                                     final int button) {
-                if (button != Input.Buttons.LEFT) {
-                    return false;
-                }
-                game.getButtonClickSFX().play();
-                clearStage(game.getButtonsUI());
-                game.getInGameScreen().getPlayer().setSpeed(game.getInGameScreen().getPlayer().getSpeed() + 10);
-                game.setScreen(game.getInGameScreen());
-                return true;
-            }
-        });
-
-
-        healthUpgrade.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
-                                     final int button) {
-                if (button != Input.Buttons.LEFT) {
-                    return false;
-                }
-                game.getButtonClickSFX().play();
-                clearStage(game.getButtonsUI());
-                game.getInGameScreen().getPlayer().setMaxHealth(game.getInGameScreen().getPlayer().getMaxHealth() + 10);
-                game.setScreen(game.getInGameScreen());
-                return true;
-            }
-        });
-
-
-        expUpgrade.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
-                                     final int button) {
-                if (button != Input.Buttons.LEFT) {
-                    return false;
-                }
-                game.getButtonClickSFX().play();
-                clearStage(game.getButtonsUI());
-                game.getInGameScreen().getPlayer().setEXPMultiplier(game.getInGameScreen().getPlayer().getEXPMultiplier() * (float) 1.1);
-                game.setScreen(game.getInGameScreen());
-                return true;
-            }
-        });
-
-
-        currencyUpgrade.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
-                                     final int button) {
-                if (button != Input.Buttons.LEFT) {
-                    return false;
-                }
-                game.getButtonClickSFX().play();
-                clearStage(game.getButtonsUI());
-                game.getInGameScreen().getPlayer()
-                        .setCurrencyMultiplier(game.getInGameScreen().getPlayer().getCurrencyMultiplier() * (float) 1.1);
-                game.setScreen(game.getInGameScreen());
-                return true;
-            }
-        });
-
-
-        attackSpeedUpgrade.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
-                                     final int button) {
-                if (button != Input.Buttons.LEFT) {
-                    return false;
-                }
-                game.getButtonClickSFX().play();
-                clearStage(game.getButtonsUI());
-                game.getInGameScreen().getPlayer().setAttackSpeed(game.getInGameScreen().getPlayer().getAttackSpeed() * (float) 0.9);
-                game.setScreen(game.getInGameScreen());
-                return true;
-            }
-        });
-
-
-        iFrameUpgrade.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
-                                     final int button) {
-                if (button != Input.Buttons.LEFT) {
-                    return false;
-                }
-                game.getButtonClickSFX().play();
-                clearStage(game.getButtonsUI());
-                game.getInGameScreen().getPlayer().setIFramesLength(game.getInGameScreen().getPlayer().getIFramesLength() * (float) 1.1);
-                game.setScreen(game.getInGameScreen());
-                return true;
-            }
-        });
-
-
-        critDamageUpgrade.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
-                                     final int button) {
-                if (button != Input.Buttons.LEFT) {
-                    return false;
-                }
-                game.getButtonClickSFX().play();
-                clearStage(game.getButtonsUI());
-                game.getInGameScreen().getPlayer().setCritMultiplier(game.getInGameScreen().getPlayer().getCritMultiplier() + 0.1f);
-                game.setScreen(game.getInGameScreen());
-                return true;
-            }
-        });
-
-        critRateUpgrade.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
-                                     final int button) {
-                if (button != Input.Buttons.LEFT) {
-                    return false;
-                }
-                game.getButtonClickSFX().play();
-                clearStage(game.getButtonsUI());
-                game.getInGameScreen().getPlayer().setCritRate(game.getInGameScreen().getPlayer().getCritRate() + 0.05f);
-                game.setScreen(game.getInGameScreen());
-                return true;
-            }
-        });
-
-
-        defenseUpgrade.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
-                                     final int button) {
-                if (button != Input.Buttons.LEFT) {
-                    return false;
-                }
-                game.getButtonClickSFX().play();
-                clearStage(game.getButtonsUI());
-                game.getInGameScreen().getPlayer().setDefense(game.getInGameScreen().getPlayer().getDefense() + 0.05f);
-                game.setScreen(game.getInGameScreen());
-                return true;
-            }
-        });
-
-
-        healthRegenUpgrade.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
-                                     final int button) {
-                if (button != Input.Buttons.LEFT) {
-                    return false;
-                }
-                game.getButtonClickSFX().play();
-                clearStage(game.getButtonsUI());
-                game.getInGameScreen().getPlayer()
-                        .setHealthRegenMultiplier(game.getInGameScreen().getPlayer().getHealthRegenMultiplier() + 0.05f);
-                game.setScreen(game.getInGameScreen());
-                return true;
-            }
-        });
+        handleAttackUpgrade();
+        handleSpeedUpgrade();
+        handleHealthUpgrade();
+        handleExpUpgrade();
+        handleCurrencyUpgrade();
+        handleAttackSpeedUpgrade();
+        handleCritDamageUpgrade();
+        handleCritRateUpgrade();
+        handleDefenseUpgrade();
+        handleHealthRegenUpgrade();
 
         return new TextButton[]{
-                this.attackUpgrade, speedUpgrade, healthUpgrade, expUpgrade, currencyUpgrade, attackSpeedUpgrade,
-                iFrameUpgrade, critDamageUpgrade, critRateUpgrade, defenseUpgrade, healthRegenUpgrade
+                this.attackUpgrade, this.speedUpgrade, this.healthUpgrade, this.expUpgrade, this.currencyUpgrade,
+                this.attackSpeedUpgrade, this.iFrameUpgrade, this.critDamageUpgrade, this.critRateUpgrade,
+                this.defenseUpgrade, this.healthRegenUpgrade
         };
     }
 
-    private void handleAttackUpdate() {
+    private void handleAttackUpgrade() {
         this.attackUpgrade.addListener(new InputListener() {
             @Override
             public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
@@ -283,4 +141,175 @@ public class UpgradeSelectionScreen implements Screen, ActorManager, Background 
             }
         });
     }
+
+    private void handleSpeedUpgrade() {
+        this.speedUpgrade.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
+                                     final int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+                game.getButtonClickSFX().play();
+                clearStage(game.getButtonsUI());
+                game.getInGameScreen().getPlayer().setSpeed(game.getInGameScreen().getPlayer().getSpeed() + 10);
+                game.setScreen(game.getInGameScreen());
+                return true;
+            }
+        });
+    }
+
+    private void handleHealthUpgrade() {
+        this.healthUpgrade.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
+                                     final int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+                game.getButtonClickSFX().play();
+                clearStage(game.getButtonsUI());
+                game.getInGameScreen().getPlayer().setMaxHealth(game.getInGameScreen().getPlayer().getMaxHealth() + 10);
+                game.setScreen(game.getInGameScreen());
+                return true;
+            }
+        });
+    }
+
+    private void handleExpUpgrade() {
+        this.expUpgrade.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
+                                     final int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+                game.getButtonClickSFX().play();
+                clearStage(game.getButtonsUI());
+                game.getInGameScreen().getPlayer().setEXPMultiplier(game.getInGameScreen().getPlayer().getEXPMultiplier() * (float) 1.1);
+                game.setScreen(game.getInGameScreen());
+                return true;
+            }
+        });
+    }
+
+    private void handleCurrencyUpgrade() {
+        this.currencyUpgrade.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
+                                     final int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+                game.getButtonClickSFX().play();
+                clearStage(game.getButtonsUI());
+                game.getInGameScreen().getPlayer().setCurrencyMultiplier(game.getInGameScreen().getPlayer().getCurrencyMultiplier() * (float) 1.1);
+                game.setScreen(game.getInGameScreen());
+                return true;
+            }
+        });
+    }
+
+    private void handleAttackSpeedUpgrade() {
+        this.attackSpeedUpgrade.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
+                                     final int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+                game.getButtonClickSFX().play();
+                clearStage(game.getButtonsUI());
+                game.getInGameScreen().getPlayer().setAttackSpeed(game.getInGameScreen().getPlayer().getAttackSpeed() * (float) 0.9);
+                game.setScreen(game.getInGameScreen());
+                return true;
+            }
+        });
+    }
+
+    private void handleIFrameUpgrade() {
+        this.iFrameUpgrade.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
+                                     final int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+                game.getButtonClickSFX().play();
+                clearStage(game.getButtonsUI());
+                game.getInGameScreen().getPlayer().setIFramesLength(game.getInGameScreen().getPlayer().getIFramesLength() * (float) 1.1);
+                game.setScreen(game.getInGameScreen());
+                return true;
+            }
+        });
+    }
+
+    private void handleCritDamageUpgrade() {
+        this.critDamageUpgrade.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
+                                     final int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+                game.getButtonClickSFX().play();
+                clearStage(game.getButtonsUI());
+                game.getInGameScreen().getPlayer().setCritMultiplier(game.getInGameScreen().getPlayer().getCritMultiplier() + 0.1f);
+                game.setScreen(game.getInGameScreen());
+                return true;
+            }
+        });
+    }
+
+    private void handleCritRateUpgrade() {
+        this.critRateUpgrade.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
+                                     final int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+                game.getButtonClickSFX().play();
+                clearStage(game.getButtonsUI());
+                game.getInGameScreen().getPlayer().setCritRate(game.getInGameScreen().getPlayer().getCritRate() + 0.05f);
+                game.setScreen(game.getInGameScreen());
+                return true;
+            }
+        });
+    }
+
+    private void handleDefenseUpgrade() {
+        this.defenseUpgrade.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
+                                     final int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+                game.getButtonClickSFX().play();
+                clearStage(game.getButtonsUI());
+                game.getInGameScreen().getPlayer().setDefense(game.getInGameScreen().getPlayer().getDefense() + 0.05f);
+                game.setScreen(game.getInGameScreen());
+                return true;
+            }
+        });
+    }
+
+    private void handleHealthRegenUpgrade() {
+        this.healthRegenUpgrade.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(final InputEvent event, final float x, final float y, final int pointer,
+                                     final int button) {
+                if (button != Input.Buttons.LEFT) {
+                    return false;
+                }
+                game.getButtonClickSFX().play();
+                clearStage(game.getButtonsUI());
+                game.getInGameScreen().getPlayer().setHealthRegenMultiplier(game.getInGameScreen().getPlayer().getHealthRegenMultiplier() + 0.05f);
+                game.setScreen(game.getInGameScreen());
+                return true;
+            }
+        });
+    }
+
 }
