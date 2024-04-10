e95416e21d3f244c237aaf0bc4d4b8a82e68e564
Jason Chow <jchow149@my.bcit.ca>
2024-04-09 22:54:59 -0700

    ADD javadocs to Charger.java

--- core/src/ca/bcit/comp2522/termproject/enemies/Charger.java
+++ core/src/ca/bcit/comp2522/termproject/enemies/Charger.java
@@ -4,15 +4,45 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * Represents the Charger class, a type of Enemy that charges towards a specified direction.
+ * Manages additional properties such as acceleration and charging movement.
+ * Inherits from the Entity class.
+ *
+ * @author Jonathan Liu
+ * @author A01375621
+ * @author jwl0724
+ * @author Jason Chow
+ * @author A00942129
+ * @author Beijzum
+ * @version 2024
+ */
 public class Charger extends Enemy {
     private final int acceleration;
     private final Vector2 chargingVector = new Vector2();
 
+    /**
+     * Constructs a Charger object with custom parameters.
+     *
+     * @param health       int representing the health points of the charger.
+     * @param acceleration int representing the acceleration value of the charger.
+     * @param attack       int representing the attack points of the charger.
+     * @param sprite       Sprite object representing the charger's sprite.
+     */
     public Charger(final int health, final int acceleration, final int attack, final Sprite sprite) {
         super(health, 0, attack, sprite);
         this.acceleration = acceleration;
     }
 
+    /**
+     * Calculates the direction vector towards a specified position and adjusts the charging behavior.
+     * Points direction vector from the Charger's current position to the specified target position.
+     * Decelerates if the charging vector and the direction vector are not aligned.
+     * Accelerates if the charging vector and the direction vector are aligned.
+     *
+     * @param x float representing the x-coordinate of the target position.
+     * @param y float representing the y-coordinate of the target position.
+     */
     @Override
     public void calculateDirectionVector(final float x, final float y) {
         this.getDirectionVector().set(x, y);
@@ -30,6 +60,10 @@ public class Charger extends Enemy {
         }
     }
 
+    /**
+     * Moves the charger based on its charging vector and speed.
+     * Updates the Charger's sprite position with the calculated delta x and delta y values.
+     */
     @Override
     public void move() {
         float angle = this.chargingVector.angleRad();
