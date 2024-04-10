acb12bf5a0db5fb70d469150450f63f0047b92b8
Jason Chow <jchow149@my.bcit.ca>
2024-04-09 22:49:41 -0700

    ADD javadocs to RangedEnemy.java

--- core/src/ca/bcit/comp2522/termproject/enemies/RangedEnemy.java
+++ core/src/ca/bcit/comp2522/termproject/enemies/RangedEnemy.java
@@ -6,7 +6,21 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 import java.util.LinkedList;
 
-public class RangedEnemy extends Enemy {
+/**
+ * Represents the RangedEnemy class.
+ * Extends the Enemy class and encapsulates the behavior and attributes of a ranged enemy.
+ * Manages attack speed, projectile firing, and specific attack properties.
+ * Inherits from the Entity class.
+ *
+ * @author Jonathan Liu
+ * @author A01375621
+ * @author jwl0724
+ * @author Jason Chow
+ * @author A00942129
+ * @author Beijzum
+ * @version 2024
+ */
+public final class RangedEnemy extends Enemy {
     private static final int SPEWER_PROJECTILE_SPEED = 130;
     private static final int SNIPER_PROJECTILE_SPEED = 500;
     private static final int BASIC_PROJECTILE_SPEED = 300;
@@ -26,30 +40,6 @@ public class RangedEnemy extends Enemy {
     private float attackTimer;
     private final Projectile projectileTemplate;
 
-    public static RangedEnemy createBasic(final float timeElapsed, final Sprite sprite,
-                                          final Sprite projectileSprite) {
-        final int health = Math.round(BASIC_BASE_HEALTH + timeElapsed / 5);
-        final int speed = Math.round(BASE_SPEED + timeElapsed / 5);
-        return new RangedEnemy(health, speed, BASIC_ATTACK, BASIC_PROJECTILE_SPEED,
-                BASIC_ATTACK_SPEED, projectileSprite, sprite);
-    }
-
-    public static RangedEnemy createSpewer(final float timeElapsed, final Sprite sprite,
-                                           final Sprite projectileSprite) {
-        final int health = Math.round(SPEWER_BASE_HEALTH + timeElapsed / 5);
-        final int speed = Math.round(BASE_SPEED + timeElapsed / 5);
-        return new RangedEnemy(health, speed, SPEWER_ATTACK, SPEWER_PROJECTILE_SPEED,
-                SPEWER_ATTACK_SPEED, projectileSprite, sprite);
-    }
-
-    public static RangedEnemy createSniper(final float timeElapsed, final Sprite sprite,
-                                           final Sprite projectileSprite) {
-        final int health = Math.round(SNIPER_BASE_HEALTH + timeElapsed / 5);
-        final int speed = Math.round(BASE_SPEED + timeElapsed / 5);
-        return new RangedEnemy(health, speed, SNIPER_ATTACK, SNIPER_PROJECTILE_SPEED,
-                SNIPER_ATTACK_SPEED, projectileSprite, sprite);
-    }
-
     private RangedEnemy(final int health, final int speed, final int attack, final int projectileSpeed,
                         final float attackSpeed, final Sprite projectileSprite, final Sprite sprite) {
         super(health, speed, attack, sprite);
@@ -59,6 +49,61 @@ public class RangedEnemy extends Enemy {
         this.attackTimer = 0;
     }
 
+    /**
+     * Creates a basic RangedEnemy with increasing health and speed over time.
+     *
+     * @param timeElapsed      float representing the time elapsed in the game.
+     * @param sprite           Sprite object representing the enemy's sprite.
+     * @param projectileSprite Sprite object representing the projectile's sprite.
+     * @return a new basic RangedEnemy object.
+     */
+    public static RangedEnemy createBasic(final float timeElapsed, final Sprite sprite,
+                                          final Sprite projectileSprite) {
+        final int health = Math.round(BASIC_BASE_HEALTH + timeElapsed / 5);
+        final int speed = Math.round(BASE_SPEED + timeElapsed / 5);
+        return new RangedEnemy(health, speed, BASIC_ATTACK, BASIC_PROJECTILE_SPEED,
+                BASIC_ATTACK_SPEED, projectileSprite, sprite);
+    }
+
+    /**
+     * Creates a spewer RangedEnemy with increasing health and speed over time.
+     *
+     * @param timeElapsed      float representing the time elapsed in the game.
+     * @param sprite           Sprite object representing the enemy's sprite.
+     * @param projectileSprite Sprite object representing the projectile's sprite.
+     * @return a new spewer RangedEnemy object.
+     */
+    public static RangedEnemy createSpewer(final float timeElapsed, final Sprite sprite,
+                                           final Sprite projectileSprite) {
+        final int health = Math.round(SPEWER_BASE_HEALTH + timeElapsed / 5);
+        final int speed = Math.round(BASE_SPEED + timeElapsed / 5);
+        return new RangedEnemy(health, speed, SPEWER_ATTACK, SPEWER_PROJECTILE_SPEED,
+                SPEWER_ATTACK_SPEED, projectileSprite, sprite);
+    }
+
+    /**
+     * Creates a sniper RangedEnemy with increasing health and speed over time.
+     *
+     * @param timeElapsed      float representing the time elapsed in the game.
+     * @param sprite           Sprite object representing the enemy's sprite.
+     * @param projectileSprite Sprite object representing the projectile's sprite.
+     * @return a new sniper RangedEnemy object.
+     */
+    public static RangedEnemy createSniper(final float timeElapsed, final Sprite sprite,
+                                           final Sprite projectileSprite) {
+        final int health = Math.round(SNIPER_BASE_HEALTH + timeElapsed / 5);
+        final int speed = Math.round(BASE_SPEED + timeElapsed / 5);
+        return new RangedEnemy(health, speed, SNIPER_ATTACK, SNIPER_PROJECTILE_SPEED,
+                SNIPER_ATTACK_SPEED, projectileSprite, sprite);
+    }
+
+    /**
+     * Fires a projectile towards the player's position.
+     *
+     * @param enemyProjectiles LinkedList of Projectile objects representing enemy projectiles.
+     * @param playerCoordX     float representing the x-coordinate of the player's position.
+     * @param playerCoordY     float representing the y-coordinate of the player's position.
+     */
     public void fireProjectile(final LinkedList<Projectile> enemyProjectiles, final float playerCoordX,
                                final float playerCoordY) {
         if (this.attackTimer >= this.attackSpeed) {
