aa5229e01a836c3349d31e638c287c1b230867d0
Jason Chow <jchow149@my.bcit.ca>
2024-04-12 21:09:44 -0700

    REFACTOR DamageNumberTest.java to have setup and split test into 2

--- tests/src/ca/bcit/comp2522/termproject/DamageNumberTest.java
+++ tests/src/ca/bcit/comp2522/termproject/DamageNumberTest.java
@@ -1,21 +1,30 @@
 package ca.bcit.comp2522.termproject;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DamageNumberTest {
 
+    private DamageNumber damageNumber;
+
+    @BeforeEach
+    public void setUp() {
+        damageNumber = new DamageNumber(100, 100, 50, false);
+    }
+
+    @Test
+    public void testIsNotExpired() {
+        assertFalse(damageNumber.isExpired());
+    }
+
     @Test
     public void testIsExpired() {
-        DamageNumber damageNumber = new DamageNumber(100, 100, 50, false);
-
-        assertFalse(damageNumber.isExpired());
-
         float defaultLifeTime = 1.0f;
-
         damageNumber.update(defaultLifeTime);
 
         assertTrue(damageNumber.isExpired());
     }
+
 }
